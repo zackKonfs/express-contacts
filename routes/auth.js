a04c6c5e5d7c0ec8007d2a9cae4e569140817c0c
zackKonfs <zack.konfs@gmail.com>
2026-04-17 17:38:39 +0800

    Day 7: login route complete — JWT auth working

--- routes/auth.js
+++ routes/auth.js
@@ -2,11 +2,14 @@ const express = require('express')
 const bcrypt = require('bcrypt')
 const router = express.Router()
 const supabase = require('../config/supabase')
+const jwt = require('jsonwebtoken')
 
 // POST register a new user
 router.post('/register', async (req , res , next) =>{
     try {
-        const {name, email, password} = req.body
+        const name = req.body.name?.trim()
+        const email = req.body.email?.trim()
+        const password = req.body.password?.trim()
 
         // validation
         if (!name || !email || !password) {
@@ -35,6 +38,43 @@ router.post('/register', async (req , res , next) =>{
 
     } catch (err) {
         return next(err)
+    }
+})
+
+// POST login
+router.post('/login', async (req, res, next)=> {
+    try {
+        const email = req.body.email?.trim()
+        const password = req.body.password?.trim()
+
+        // validation
+        if (!email || !password) return res.status(400).json({error:'Email and password are required'})
+
+        // gate 1 - does this email exist?
+        const {data: user, error} = await supabase
+            .from('users')
+            .select('id, name, email, password')
+            .eq('email', email)
+            .single()
+
+        if (error || !user) return res.status(401).json({error:'Invalid credentials'})
+        
+        //gate 2 - does the password match?
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if (!isMatch) return res.status(401).json({error:'Invalid credentials'})
+
+        // generate JWT
+        const token = jwt.sign(
+            {id: user.id, email: user.email},
+            process.env.JWT_SECRET,
+            {expiresIn: '7d'}
+        )
+
+        res.status(200).json({token})
+
+    } catch (err) {
+       return next (err)
     }
 })
 
